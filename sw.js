126a359b41b83f51d72738a5e22ccec80c39aa02
Dakalo-cmd <mathagudakalo362@gmail.com>
2026-05-15 09:58:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
+const CACHE_NAME = "mountolive-cache-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./MountOlive Logo.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
